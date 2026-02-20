746dcdb3819e8f177bc2c5d5fa40e92b358410a3
Dabbouniiheb <dabbouniiheb@gmail.com>
2026-02-20 21:58:17 +0100

    delete comment

--- js/main1.js
+++ js/main1.js
@@ -1,29 +1,3 @@
-/*let searchBtn = document.querySelector("#search-btn");
-let searchBar = document.querySelector(".search-bar-container");
-let formBtn = document.querySelector("#login-btn");
-let loginForm = document.querySelector(".login-form-container");
-let formClose = document.querySelector("#form-close");
-
-window.onscroll = () =>{
-    searchBtn.classlist.remove("fa-times");
-    searchBtr.classlist.remove("active");
-}
-searchBtn.addEventListener("click",(),=>{
-    searchBtn.classlist.toggle("fa-times");
-    searchBtr.classlist.toggle("active");
-});
-
-formBtn.addEventListener("click",(),=>{
-    
-    searchBtr.classlist.add("active");
-});
-
-formClose.addEventListener("click",(),=>{
-    
-    searchBtr.classlist.remove("active");
-});*/
-
-
 var swiper = new Swiper(".review-slider", {
     spaceBetween:20,
     loop:true,
